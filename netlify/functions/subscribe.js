cbc45f9d2c1e6daa0b1abe075149b065437c489e
johor9610 <johor9610@gmail.com>
2026-03-30 11:59:49 -0500

    fix: update Netlify function for node-fetch and Node 18

--- netlify/functions/subscribe.js
+++ netlify/functions/subscribe.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 exports.handler = async (event) => {
   console.log('[subscribe] Method:', event.httpMethod);
   console.log('[subscribe] Headers:', JSON.stringify(event.headers));
