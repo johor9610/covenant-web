5dd017304f1e479f90b63c4f8c5adcd824eadd46
johor9610 <johor9610@gmail.com>
2026-03-30 19:21:29 -0500

    remove node-fetch, use native fetch Node 18

--- netlify/functions/subscribe.js
+++ netlify/functions/subscribe.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch');
-
 exports.handler = async (event) => {
   console.log('[subscribe] Method:', event.httpMethod);
   console.log('[subscribe] Headers:', JSON.stringify(event.headers));
